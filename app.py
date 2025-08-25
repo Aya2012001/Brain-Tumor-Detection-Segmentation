51a1169be5df0f147fe92a28c5d1810a503a502c
Aya2012001 <116909087+Aya2012001@users.noreply.github.com>
2025-08-25 19:42:01 +0300

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def load_detection_model():
         repo_id="Aya2012001/Brain_Tumor_Detection_Segmentation",
         filename="brain_tumor_detection_best_model.keras"
     )
-    model = tf.keras.models.load_model(model_path)
+    model = tf.keras.models.load_model(model_path, compile=False)  # 👈 important
     return model
 
 @st.cache_resource
@@ -19,7 +19,7 @@ def load_segmentation_model():
         repo_id="Aya2012001/Brain_Tumor_Detection_Segmentation",
         filename="Brain_Tumor_Segmentation_UNet.keras"
     )
-    model = tf.keras.models.load_model(model_path)
+ model = tf.keras.models.load_model(model_path, compile=False)  # 👈 important
     return model
 
 def preprocess_for_detection(image: Image.Image):
