c4df810258a6587671393a669f847880222fbec1
Aya2012001 <116909087+Aya2012001@users.noreply.github.com>
2025-08-25 19:46:56 +0300

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def load_segmentation_model():
         repo_id="Aya2012001/Brain_Tumor_Detection_Segmentation",
         filename="Brain_Tumor_Segmentation_UNet.keras"
     )
- model = tf.keras.models.load_model(model_path, compile=False)  # 👈 important
+    model = tf.keras.models.load_model(model_path, compile=False)  # 👈 important
     return model
 
 def preprocess_for_detection(image: Image.Image):
@@ -31,34 +31,4 @@ def preprocess_for_detection(image: Image.Image):
 def preprocess_for_segmentation(image: Image.Image):
     img = image.resize((128, 128)) 
     img_array = np.array(img) / 255.0
-    img_array = np.expand_dims(img_array, axis=0)
-    return img_array
-
-st.title("🧠 Brain Tumor Detection & Segmentation")
-uploaded_file = st.file_uploader("Upload an MRI image", type=["jpg", "jpeg", "png"])
-
-if uploaded_file is not None:
-    image = Image.open(uploaded_file).convert("RGB")
-    st.image(image, caption="Uploaded MRI", use_column_width=True)
-
-  
-    st.subheader("Step 1: Detection")
-    detection_model = load_detection_model()
-    processed_det = preprocess_for_detection(image)
-    prediction = detection_model.predict(processed_det)[0]
-
-    if prediction[0] > 0.5:
-        st.error("⚠️ Tumor Detected")
-
-      
-        st.subheader("Step 2: Segmentation")
-        seg_model = load_segmentation_model()
-        processed_seg = preprocess_for_segmentation(image)
-        mask = seg_model.predict(processed_seg)[0]
-
-        mask = (mask.squeeze() > 0.5).astype(np.uint8) * 255
-
-        st.image(mask, caption="Segmentation Mask", use_column_width=True, clamp=True)
-
-    else:
-        st.success("✅ No Tumor Detected")
+    img_array = np.expand_dims(img_ar_
